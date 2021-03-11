9531e1376ccef411a7ecf9d06fa7ee532152e2b9
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-03-11 23:58:52 +0000

    Added Predicate, Fucntion and Consumer code

--- src/main/java/com/techsharezone/javafunctionalprogramming/functionalinterface/FunctionalInterfaceExample.java
+++ src/main/java/com/techsharezone/javafunctionalprogramming/functionalinterface/FunctionalInterfaceExample.java
@@ -10,9 +10,51 @@ public class FunctionalInterfaceExample {
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(2, 3, 5, 6, 7, 8);
+
+        Predicate<Integer> predicateLambda = x -> x%2==0;
+
+        Predicate<Integer> predicate = new Predicate<Integer>() {
+
+            @Override
+            public boolean test(Integer x) {
+                return x%2==0;
+            }
+        };
+
+        Function<Integer, Integer> functionLambda = x-> x *x;
+
+        Function<Integer, Integer> function = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer x) {
+                return x * x;
+            }
+        };
+
+        Consumer<Integer> consumerLambda = System.out::println;
+
+        Consumer<Integer> consumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer x) {
+                System.out.println(x);
+            }
+        };
+
+        //Using PredicateLambda
         numbers.stream()
-                .filter(x->x%2==0) // Predicate - Represents a predicate (boolean-valued function) of one argument.
-                .map(x -> x*x) //Function - Represents a function that accepts one argument and produces a result.
-                .forEach(System.out::println); //Consumer - Represents an operation that accepts a single input argument and returns no result.
+                .filter(predicateLambda)
+                .map(functionLambda)
+                .forEach(consumerLambda);
+
+        //Using PredicateLambda
+        numbers.stream()
+                .filter(predicate)
+                .map(function)
+                .forEach(consumer);
+
+        //Using pure lambda expression
+        numbers.stream()
+                .filter(x -> x%2 ==0)
+                .map(x -> x *x)
+                .forEach(System.out::println);
     }
 }

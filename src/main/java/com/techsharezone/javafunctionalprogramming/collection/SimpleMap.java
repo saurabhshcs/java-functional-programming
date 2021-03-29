8f06d04be3f5919322f6b351d240be978332ee83
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-03-29 23:46:48 +0100

    Updated code and package structure12

--- src/main/java/com/techsharezone/javafunctionalprogramming/collection/SimpleMap.java
+++ src/main/java/com/techsharezone/javafunctionalprogramming/collection/SimpleMap.java
@@ -14,13 +14,23 @@ import java.util.Optional;
 public class SimpleMap {
 
     public static void main(String[] args) {
+
         Map<String, String> map1 = new HashMap<>();
-        map1.put("firstRegDate", "29-03-2021");
+        map1.put("firstRegistrationDate", "30-03-2021");
 
-        Map<String, Map> map = new HashMap<>();
-        map.put("data", map1);
+        Map<String, Map<String, String>> map2 = new HashMap<>();
+        map2.put("vehicleByArn", map1);
 
-        Optional.of(map.get("data")).get("firstRegDate");
+        Map<String, Map<String, Map<String, String>>> messageBody = new HashMap<>();
+        messageBody.put("data" ,map2);
+
+
+        System.out.println(Optional.of(messageBody.get("data").get("vehicleByArn"))
+                .flatMap( x -> Optional.of(x.get("firstRegistrationDate")))
+                .map(firstRegDate -> LocalDate.parse(firstRegDate))
+                .or(()-> Optional.empty()));
+
+
 
     }
 }

44fface8b694e5678187b1ab0b8206546aabbab6
SAURABH SHARMA <saurabhshcs@gmail.com>
2021-06-16 23:31:22 +0100

    [tsz-functional-interface-random-example] ss - Created an exmaple class for random functional interface practice.

---
+++ src/main/java/com/techsharezone/javafunctionalprogramming/functionalinterface/random/FunctionalInterfaceExample.java
@@ -0,0 +1,12 @@
+package com.techsharezone.javafunctionalprogramming.functionalinterface.random;
+
+/*
+ * @created 16/06/2021 -23:29
+ * @project java-functional-programming
+ * @author  saurabhshcs
+ */
+
+public class FunctionalInterfaceExample {
+
+
+}
